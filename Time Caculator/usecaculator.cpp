b036c41072b9e46774c89c5170bfbeb06b97bab4
EuclidLv <121611226+EuclidLv@users.noreply.github.com>
2023-07-16 17:27:02 +0800

    Add files via upload

---
+++ Time Caculator/usecaculator.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "timecaculator.h"
+
+int main()
+{
+    using std::cout;
+    using std::endl;
+    Time aida(3, 25);
+    Time tosca(2, 48);
+    Time temp;
+    cout << "Aida and tosca:\n";
+    cout << aida << "; " << tosca << endl;
+    temp = aida + tosca;
+    cout << "Aida + Tosca: " << temp << endl;
+    temp = aida * 1.17;
+    cout << "Aida * 1.17" << temp << endl;
+    cout << "10.0 * tosca" << 10.0 * tosca << endl;
+    return 0;
+}
